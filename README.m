80a027916959c18f2f2976820a70c080e7a9a83d
josinvt <josefinavogt2006@gmail.com>
2026-03-15 20:00:13 -0600

    Update README.m

--- README.m
+++ README.m
@@ -1,2 +1,49 @@
-# judicial-stylometry-analysis
-Stylometric analysis of civil and criminal court judgments using R
+# Stylometric Analysis of German Court Judgments
+
+## Project
+
+This project investigates stylistic differences between civil law and criminal law court judgments in Germany.
+
+The goal is to determine whether stylometric features can be used to automatically distinguish between these two legal domains.
+
+## Research Question
+
+Can linguistic features be used to identify whether a court judgment belongs to civil or criminal law?
+
+## Data
+
+The dataset consists of publicly available German court decisions from OpenLegalData.
+
+https://openlegaldata.io
+
+Due to licensing restrictions the raw data is not included.
+
+## Methods
+
+The analysis uses stylometric methods including:
+
+- word frequency analysis
+- sentence length statistics
+- principal component analysis (PCA)
+
+All analyses were implemented in R.
+
+## Repository Structure
+
+data/ – dataset description  
+scripts/ – R analysis scripts  
+results/ – generated plots and outputs  
+paper/ – project report  
+
+## Reproducibility
+
+To reproduce the analysis:
+
+1 Download the dataset  
+2 Place the data in the /data folder  
+3 Run the R scripts in the /scripts folder
+
+## Author
+
+Josefina Vogt  
+Digital Humanities Project
