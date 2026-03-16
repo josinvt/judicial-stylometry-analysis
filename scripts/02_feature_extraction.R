bc6df4ec12b35cbba93d89eb3bac62b985d0b628
josinvt <josefinavogt2006@gmail.com>
2026-03-15 19:56:26 -0600

    Update 02_feature_extraction.R

--- scripts/02_feature_extraction.R
+++ scripts/02_feature_extraction.R
@@ -1,7 +1,12 @@
 library(tidytext)
+library(dplyr)
 
-tokens <- judgments %>%
+data <- read.csv("data/judgments_clean.csv")
+
+tokens <- data %>%
   unnest_tokens(word, text)
 
 word_counts <- tokens %>%
   count(word, sort = TRUE)
+
+print(head(word_counts))
